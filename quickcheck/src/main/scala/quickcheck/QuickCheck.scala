af047e1985d87affc0b957d82410bafd4fdfe5f3
Andrii Lohvynchuk <logan.zhi@gmail.com>
2023-01-22 16:30:07 +0200

    Complete quickcheck

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -10,14 +10,13 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap :
   lazy val nonEmptyHeap: Gen[H] =
     for
       num <- arbitrary[Int]
-      heap <- oneOf(const(empty), genHeap)
+      heap <- frequency((1, const(empty)), (4, nonEmptyHeap))
     yield
       insert(num, heap)
 
-  lazy val genHeap: Gen[H] = frequency((1, const(empty)), (3, nonEmptyHeap))
+  lazy val genHeap: Gen[H] = oneOf(const(empty), nonEmptyHeap)
 
   given Arbitrary[H] = Arbitrary(genHeap)
-
   property("gen1") = forAll { (h: H) =>
     classify(isEmpty(h), "empty") {
       val m = if isEmpty(h) then 0 else findMin(h)
@@ -25,51 +24,50 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap :
   }
 
   property("findMin returns the smallest of two elements") = forAll { (a: Int, b: Int, h: H) =>
-    classify(isEmpty(h), "empty") {
-      isEmpty(h) ==> {
-        val afterA = insert(a, h)
-        val afterB = insert(b, afterA)
-        findMin(afterB) == Math.min(a, b)
-      }
+    isEmpty(h) ==> {
+      val afterA = insert(a, h)
+      val afterB = insert(b, afterA)
+      findMin(afterB) == Math.min(a, b)
     }
   }
 
   property("insert into empty then deleteMin results in an empty heap") = forAll { (x: Int, h: H) =>
-    classify(isEmpty(h), "empty") {
-      isEmpty(h) ==> {
-        val singleton = insert(x, h)
-        isEmpty(deleteMin(singleton))
-      }
+    isEmpty(h) ==> {
+      val singleton = insert(x, h)
+      isEmpty(deleteMin(singleton))
     }
   }
 
-  property("idk wtf") = forAll { (x: Int, h: H) => (isEmpty(h)) ==> true }
+  property("successive deleteMin should not skip elements") = forAll { (a: Int, b: Int, c: Int) =>
+    val max = List(a, b, c).max
+    val afterInsert = insert(c, insert(b, insert(a, empty)))
+    val heap = deleteMin(deleteMin(afterInsert))
+    findMin(heap) == max
+  }
 
   def heap2list(h: H): List[Int] = {
     def traverseRec(h: H, result: List[Int]): List[Int] = {
       if (isEmpty(h)) result.reverse
       else traverseRec(deleteMin(h), findMin(h) :: result)
     }
+
     traverseRec(h, Nil)
   }
 
-  property("calls to findMin return elements in ordered manner") = forAll { (h: H) =>
-    (!isEmpty(h)) ==> {
-      def popOneAndCompare(prev: Int, heap: H): Boolean =
-        if (isEmpty(heap)) true
-        else {
-          val next = findMin(heap)
-          if (next >= prev) popOneAndCompare(next, deleteMin(heap)) else false
-        }
-      popOneAndCompare(Integer.MIN_VALUE, h)
+  property("calls to findMin return elements in ordered manner") = forAll(nonEmptyHeap) { (h: H) =>
+    def popOneAndCompare(prev: Int, heap: H): Boolean = {
+      if (isEmpty(heap)) true
+      else {
+        val curr = findMin(heap)
+        if (curr >= prev) popOneAndCompare(curr, deleteMin(heap)) else false
+      }
     }
+    popOneAndCompare(Integer.MIN_VALUE, h)
   }
 
-  property("minimum of meld is one of two findMin") = forAll { (h1: H, h2: H) =>
-    (!isEmpty(h1) && !isEmpty(h2)) ==> {
+  property("minimum of meld is one of two findMin") = forAll(nonEmptyHeap, nonEmptyHeap) { (h1: H, h2: H) =>
       val min1 = findMin(h1)
       val min2 = findMin(h2)
       val combined = meld(h1, h2)
       findMin(combined) == min1 || findMin(combined) == min2
-    }
   }
